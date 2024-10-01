5618f0956bcbfa548b5510b209f320e111b4d961
Ashba Aslam <ashbaaslam6@gmail.com>
2024-10-02 00:43:15 +0500

    Add files via upload

---
+++ chap 1&2/index.js
@@ -0,0 +1,28 @@
+// chapter 01
+// alert
+alert ("WELLCOME TO MY WEBSITE");
+alert("HELLO WORLD");
+// CHAPTER 02
+// declare a variable
+
+var x = "Hello World";
+alert(x);
+
+// declare a variable age
+
+var age; 
+age = 20;
+alert(age);
+// declare a variable greeting
+
+var greeting = "Good Morning";
+alert(greeting);
+
+greeting = "Good evening";
+alert (greeting)
+
+// declare a variable color
+
+var color ="Black";
+
+// alert(My Favorite color is + color + ".");
